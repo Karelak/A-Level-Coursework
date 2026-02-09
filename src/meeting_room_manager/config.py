1cbc8d1541f36952a31585529a7a20b9c17d4e61
Kaarel <partkaarel@gmail.com>
2026-02-09 18:33:09 +0000

    feat: Configure PostgreSQL settings and update application to use Postgres_Config

--- src/meeting_room_manager/config.py
+++ src/meeting_room_manager/config.py
@@ -9,9 +9,7 @@ class Config:
     MAILJET_API_KEY = os.getenv("MAILJET_API_KEY")
     MAILJET_API_SECRET = os.getenv("MAILJET_API_SECRET")
     MAILJET_DEFAULT_SENDER = os.getenv("MAILJET_DEFAULT_SENDER")
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "SQLALCHEMY_DATABASE_URI", "sqlite:///meeting_rooms.db"
-    )
+    SQLALCHEMY_DATABASE_URI = "sqlite:///meeting_rooms.db"
     SECRET_KEY = os.getenv("SECRET_KEY", os.urandom(24))
     WTF_CSRF_ENABLED = True
     WTF_CSRF_TIME_LIMIT = None
@@ -19,3 +17,14 @@ class Config:
     SESSION_COOKIE_SECURE = False  # Set to True in production with HTTPS
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = "Lax"
+
+
+class Postgres_Config(Config):
+    PG_HOST = os.getenv("PG_HOST", "localhost")
+    PG_PORT = os.getenv("PG_PORT", 5432)
+    PG_USER = os.getenv("PG_USER", "user")
+    PG_PASSWORD = os.getenv("PG_PASSWORD", "password")
+    PG_DB = os.getenv("PG_DB", "mydb")
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}"
+    )
