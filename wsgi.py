8d0ebbe5f3afe769aa536d45106c0ab9b3f5d5ab
Kaarel <partkaarel@gmail.com>
2026-02-21 20:35:03 +0000

    feat: Add waitress dependency and update wsgi.py to specify host and port for Flask app

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,5 @@ from src.meeting_room_manager import create_app
 
 app = create_app()
 if __name__ == "__main__":
-    app.run(debug=True)
+    # flask will be run whenever this file gets executed directly so might as well use debug=true as flask is only used for development and testing
+    app.run(debug=True, host="127.0.0.1", port=8001)
